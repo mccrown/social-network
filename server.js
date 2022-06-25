19a711c41d3ed803b8189b466177bb20501055f7
CMcCown <connermccown94@gmail.com>
2022-06-25 08:26:11 -0600

    server.js updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || '', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 }); 
